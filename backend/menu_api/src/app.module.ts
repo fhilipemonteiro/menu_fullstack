1df27f0f6615ed09c76fa13fb3533aadbae3bcab
Fhilipe Monteiro <fhilipemonteiro@hotmail.com>
2024-02-10 20:35:47 -0300

    feat: add typeorm, pg, and establishing connection with database

--- backend/menu_api/src/app.module.ts
+++ backend/menu_api/src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import 'dotenv/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.HOST_DB,
+      port: Number(process.env.PORT_DB),
+      username: process.env.USERNAME_DB,
+      password: process.env.PASSWORD_DB,
+      database: process.env.DATABASE_DB,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
