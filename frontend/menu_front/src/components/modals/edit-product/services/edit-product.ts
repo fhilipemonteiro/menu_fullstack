7b908d5ecb7c9b6e96aba3b423f57ab05fb230c3
Fhilipe Monteiro <fhilipemonteiro@hotmail.com>
2024-02-21 00:13:21 -0300

    fix: remove console.log of code

--- frontend/menu_front/src/components/modals/edit-product/services/edit-product.ts
+++ frontend/menu_front/src/components/modals/edit-product/services/edit-product.ts
@@ -8,8 +8,6 @@ interface IProduct {
 }
 
 export const updateProduct = async (id: string, data: IProduct, accessToken: string) => {
-  console.log(data);
-
   const response = await axios.patch(`http://localhost:3000/products/${id}`, data, {
     headers: {
       'Content-Type': 'application/json',
